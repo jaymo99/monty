59a3d88f09cfa4027f02a4c75091ac752d7dedc4
James Kariuki <karisjaymo99@gmail.com>
2023-03-22 07:47:26 +0300

    add 'monty.h' header file

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * main - main entry point of the program
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return - Always SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	stack_t *
+	instruction_t push;
+	instruction_t pall;
+
+	push->opcode = "push";
+	push->f = push();
+	pall->opcode = "pall";
+	pall->f = pall();
+
+	return (EXIT_SUCCESS);
+}
